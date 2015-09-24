5e02b8522663dfb82604816e66478a9e0cfeb73a
Oleg Ivanov <morhekil@morhekil.net>
2015-09-25 01:50:26 +1000

    initial socket management code

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
+'use strict';
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http, { path: '/redogram' });
 
-app.get('/', (req, res) => {
-  res.send('Chloe, open the socket!');
-});
-
-io.on('connection', (socket) => {
-  console.log('user connected');
-  setInterval(() => {
-    var amount = Math.floor(Math.random() * 10000) / 100;
-    console.log('sending balance ', amount);
-    socket.emit('player.balance', { amount: amount });
-  }, 1000);
-});
+// attach connection manager to HTTP server instance
+var mgr = require('./lib/manager')(io);
 
 var port = 3001;
+app.get('/', (req, res) => { res.send('Chloe, open the socket!'); });
 http.listen(port, () => { console.log('socket server started on *:' + port); });
