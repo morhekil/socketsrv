af0215ff5d5fbe95829a0c6f4448486c78e2b18b
Oleg Ivanov <morhekil@morhekil.net>
2015-09-26 00:03:05 +1000

    AMQP proxy prototype, hard-coded with balance related queues atm

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 'use strict';
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http, { path: '/redogram' });
+var app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http, { path: '/redogram' });
 
-// attach connection manager to HTTP server instance
-var mgr = require('./lib/manager')(io);
+var amqp = require('amqp');
+// connect to AMQP
+var mqcon = amqp.createConnection();
 
-var port = 3001;
-app.get('/', (req, res) => { res.send('Chloe, open the socket!'); });
-http.listen(port, () => { console.log('socket server started on *:' + port); });
+// waiting until the connection is established, then proceed
+mqcon.on('ready', () => {
+  console.log('connected to amqp');
+
+  // attach connection manager to HTTP server instance
+  var mgr = require('./lib/manager')(io, mqcon);
+
+  var port = 3001;
+  app.get('/', (req, res) => { res.send('Chloe, open the socket!'); });
+  http.listen(port,
+              () => { console.log('socket server started on *:' + port); });
+});
