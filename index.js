b2252af09d2d72be4bfd221f827d5a5de366d604
Oleg Ivanov <morhekil@morhekil.net>
2015-09-27 23:57:54 +1000

    cleaned up proxy launcher script

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 'use strict';
 
+const
+    DEFAULT_PORT = 0,
+    DEFAULT_SOCKET_PATH = '/redogram',
+    DEFAULT_AMQP = 'amqp://localhost';
+
 var process = require('process'),
+    sockpath = process.env.SOCKET_PATH || DEFAULT_SOCKET_PATH,
+    amqp_url = process.env.AMQP_URL || DEFAULT_AMQP,
+    http_port = process.env.PORT || DEFAULT_PORT;
+
+var amq = require('amq'),
     app = require('express')(),
     http = require('http').Server(app),
-    io = require('socket.io')(http, { path: '/redogram' });
+    io = require('socket.io')(http, { path: sockpath });
 
-var amq = require('amq');
 // connect to AMQP
 var mqcon = amq.createConnection(
   { host: 'localhost', debug: true },
   { reconnect : { strategy : 'constant' , initial : 1000 } }
 );
 
-const DEFAULT_PORT = 3001;
+// attach connection manager to HTTP server instance
+var mgr = require('./lib/manager')(io, mqcon);
 
-var work = function() {
-  // attach connection manager to HTTP server instance
-  var mgr = require('./lib/manager')(io, mqcon);
-
-  app.get('/', (req, res) => { res.send('Chloe, open the socket!'); });
-  var port = process.env.PORT || DEFAULT_PORT;
-  http.listen(port,
-              () => { console.log('socket server started on *:' + port); });
-};
-work();
-
-// waiting until the connection is established, then proceed
-// mqcon.on('ready', () => {
-//   console.log('connected to amqp');
-//   try {
-//     work();
-//   } catch (err) {
-//     console.log(err);
-//     process.exit(-1);
-//   }
-// });
+app.get('/', (req, res) => { res.send('Chloe, open the socket!'); });
+http.listen(http_port);
+http.on('listening',
+        () => { console.log('socket server started on *:' +
+                            http.address().port); });
