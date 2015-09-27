8499cf1575c54d261e14a2f6b3592371b6bd4fdb
Oleg Ivanov <morhekil@morhekil.net>
2015-09-27 23:42:41 +1000

    generalised queue implementation, and migrated to amq

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 'use strict';
 
-var app = require('express')(),
+var process = require('process'),
+    app = require('express')(),
     http = require('http').Server(app),
     io = require('socket.io')(http, { path: '/redogram' });
 
-var amqp = require('amqp');
+var amq = require('amq');
 // connect to AMQP
-var mqcon = amqp.createConnection();
+var mqcon = amq.createConnection(
+  { host: 'localhost', debug: true },
+  { reconnect : { strategy : 'constant' , initial : 1000 } }
+);
 
-// waiting until the connection is established, then proceed
-mqcon.on('ready', () => {
-  console.log('connected to amqp');
+const DEFAULT_PORT = 3001;
 
+var work = function() {
   // attach connection manager to HTTP server instance
   var mgr = require('./lib/manager')(io, mqcon);
 
-  var port = 3001;
   app.get('/', (req, res) => { res.send('Chloe, open the socket!'); });
+  var port = process.env.PORT || DEFAULT_PORT;
   http.listen(port,
               () => { console.log('socket server started on *:' + port); });
-});
+};
+work();
+
+// waiting until the connection is established, then proceed
+// mqcon.on('ready', () => {
+//   console.log('connected to amqp');
+//   try {
+//     work();
+//   } catch (err) {
+//     console.log(err);
+//     process.exit(-1);
+//   }
+// });
